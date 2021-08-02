268ef8a1a9854e73db688601b651b7703d5daa61
Henry Schmid <henry.schmid1@gmail.com>
2021-08-02 13:11:29 -0600

    Create initial tests for food trucks and items

---
+++ spec/food_truck_spec.rb
@@ -0,0 +1,16 @@
+require './lib/item'
+require './lib/food_truck'
+
+RSpec.describe FoodTruck do
+  context 'I1' do
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+    it 'exists and has attributes' do
+      expect(food_truck).to be_a(FoodTruck)
+      expect(food_truck.name).to eq("Rocky Mountain Pies")
+      expect(food_truck.inventory).to eq({})
+    end
+  end
+end
